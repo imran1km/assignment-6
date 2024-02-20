f02a3f967d9f282059864f55aac939a1f233e236
imran1km <160271645+imran1km@users.noreply.github.com>
2024-02-20 23:39:48 +0500

    Add files via upload

---
+++ main6.js
@@ -0,0 +1,4 @@
+var whitespaceName = "\n\t Imran Memon\n\t";
+console.log(whitespaceName);
+var withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
---
+++ main6.ts
@@ -0,0 +1,6 @@
+let whitespaceName = "\n\t Imran Memon\n\t";
+console.log(whitespaceName)
+
+let withoutwhitespaceName = whitespaceName.trim();
+
+console.log(withoutwhitespaceName);
